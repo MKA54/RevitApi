d386a50a10e97948cd58f9406c7333cc60b2b3a4
Mka91 <Mka91@HUAWEI>
2022-06-06 21:58:52 +0700

    revit

---
+++ ExportToImage/Main.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportToImage
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiApplication = commandData.Application;
+            var uiDoc = uiApplication.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            using (var ts = new Transaction(doc, "export IMAGE"))
+            {
+                ts.Start();
+
+                var img = new ImageExportOptions();
+
+                img.ExportRange = ExportRange.CurrentView;
+                img.HLRandWFViewsFileType = ImageFileType.PNG;
+
+                var DEsktoppath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                img.FilePath = DEsktoppath + @"\" + doc.ActiveView.Name;
+                img.ShadowViewsFileType = ImageFileType.PNG;
+
+                doc.ExportImage(img);
+
+                ts.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ExportToNWC/Main.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportToNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiApplication = commandData.Application;
+            var uiDoc = uiApplication.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            using (var ts = new Transaction(doc, "export NWC"))
+            {
+                ts.Start();
+
+                var nwcOption = new NavisworksExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc",
+                    nwcOption);
+
+                ts.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
